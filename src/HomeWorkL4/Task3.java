a210778dc5699c1a312d70128a4f824382cf13da
AlexanderTarasevich <tarasbars0000@mail.ru>
2024-03-30 22:16:04 +0300

    Initial commit

--- src/HomeWorkL4/Task3.java
+++ src/HomeWorkL4/Task3.java
@@ -1,7 +1,17 @@
 package HomeWorkL4;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Введите элемент массива: ");
+            array[i] = scanner.nextInt();
+        }
+
     }
 }
